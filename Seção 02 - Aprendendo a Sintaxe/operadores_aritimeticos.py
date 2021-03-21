29af864ff9278aa7be2fcdbeaa8c9b762ac4b0ca
romulovieira777 <romulo.vieira777@gmail.com>
2021-03-21 13:37:24 -0300

    Aprendendo a Sintaxe

---
+++ Seção 02 - Aprendendo a Sintaxe/operadores_aritimeticos.py
@@ -0,0 +1,34 @@
+# Sum (Soma)
+adding = 10 + 10  # Somando
+
+# Subtraction (Subtração)
+subtracting = 10 - 2  # Subtraindo
+
+# Multiplication (Multiplicação)
+multiplying = 10 * 5  # Multiplicando
+
+# Division (Divisão)
+dividing = 10 / 2  # Dividindo
+
+# Wattage (Potência)
+wattage = 2 ** 4
+
+# Rest of a division (Resto de uma divisão)
+rest_01 = 15 % 2  # Resto 01
+rest_02 = 15 % 1  # Resto 02
+
+number_01 = 7  # Número 01
+number_02 = 9
+number_03 = 8
+number_04 = 9
+
+average = (number_01 + number_02 + number_03 + number_04) / 4  # Média Aritímética
+
+print(adding)
+print(subtracting)
+print(multiplying)
+print(dividing)
+print(wattage)
+print(rest_01)
+print(rest_02)
+print(average)
