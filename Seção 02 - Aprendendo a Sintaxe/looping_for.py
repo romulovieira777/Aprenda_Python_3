1112b8cf2d05d57dd0bc2985999b0306126852ed
romulovieira777 <romulo.vieira777@gmail.com>
2021-03-23 07:55:10 -0300

    Aprendendo a Sintaxe

---
+++ Seção 02 - Aprendendo a Sintaxe/looping_for.py
@@ -0,0 +1,29 @@
+# Looping for
+for x in range(10):
+    print(x)
+
+
+for x in range(1, 10):
+    print(x)
+
+
+for x in range(0, 10, 3):
+    print(x)
+
+
+cars = ['Camaro', 'Ranger', 'Pajero', 'Hilux']  # Carros
+
+for car in cars:
+    print(car)
+
+
+name = 'José'
+print(len(name))
+
+
+for a in range(len(cars)):
+    print(a)
+
+
+for a in range(len(cars)):
+    print(cars[a])
