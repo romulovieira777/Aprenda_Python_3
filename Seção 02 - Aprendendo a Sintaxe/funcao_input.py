cbae131ca49cfa76a0c10a1e9ab402b8dcd0dbf2
romulovieira777 <romulo.vieira777@gmail.com>
2021-03-23 08:35:10 -0300

    Aprendendo a Sintaxe

---
+++ Seção 02 - Aprendendo a Sintaxe/funcao_input.py
@@ -0,0 +1,24 @@
+# Data entry via the console (Entrada de dados através do console)
+
+# Input function (Função input)
+
+name = 'José da Silva'  # nome
+print(name)
+
+name = input('Type your name: ')  # Digite seu nome
+print(name)
+
+
+# The input function always returns a string (A função input sempre retorna uma string)
+# int()
+# float()
+# str()
+
+print('Add two numbers')  # Somar dois números
+a = int(input('Enter the first number: '))  # Digite o primeiro número
+b = int(input('Enter the second number: '))  # Digite o segundo número
+
+soma = a + b
+
+print('The sum of the number', a, 'with the number', b, "it's the same as", soma)
+# A soma do número a com o número b é igual a
