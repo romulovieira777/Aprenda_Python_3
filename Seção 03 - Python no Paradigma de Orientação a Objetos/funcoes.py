ec12f788414c8cd0fe019f90372a17e868b5758c
romulovieira777 <romulo.vieira777@gmail.com>
2021-03-24 08:41:42 -0300

    Python no Paradigma de Orientação a Objetos

---
+++ Seção 03 - Python no Paradigma de Orientação a Objetos/funcoes.py
@@ -0,0 +1,19 @@
+def soma():
+    print('Welcome to the declaration of a function')
+    # Bem vindo a declaração de uma função
+
+
+def soma_01(x, y):
+    result = x + y
+    print(result)  # Resultado
+
+
+soma()
+soma_01(10, 87458)
+
+
+def subtraction(a, b):
+    return a - b
+
+
+print(subtraction(78, 54))
