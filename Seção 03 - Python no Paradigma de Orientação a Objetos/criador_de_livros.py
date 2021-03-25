f60419c8048d45ba56c8757c87e21415701f3351
romulovieira777 <romulo.vieira777@gmail.com>
2021-03-25 08:26:55 -0300

    Python no Paradigma de Orientação a Objetos

---
+++ Seção 03 - Python no Paradigma de Orientação a Objetos/criador_de_livros.py
@@ -0,0 +1,13 @@
+from classes import Book
+
+book_01 = ('Python course in 6h', 'Alcimar Costa', 'Udemy', '878-98-9988-988-9', 2018)
+# Curso de Python em 6h
+book_02 = ('Python for web', 'Alcimar Costa', 'Udemy', '999-99-999-9999-9', 2018)
+# Python para web
+book_03 = ('Artificial intelligence', 'Alcimar Costa', 'Udemy', '888-88-888-8888-8', 2018)
+# Inteligência Artificial
+
+books = [book_01, book_02, book_03]
+
+for l in books:
+    print(l)
