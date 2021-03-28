6de9b8ffe7a0325ca9ca6b8742a6883e31254eed
romulovieira777 <romulo.vieira777@gmail.com>
2021-03-28 11:20:33 -0300

    Módulo Extra - Criando um Sistema Web com Django

---
+++ Seção 04 - Módulo Extra - Criando um Sistema Web com Django/localhost.py
@@ -0,0 +1,25 @@
+# http://127.0.0.1:8000/
+
+'''
+/* Subindo a Aplicação */
+python manage.py runserver
+
+/* Criando um Banco de Dados */
+python manage.py migrate
+
+/* Criando um Superusuario */
+python manage.py createsuperuser
+
+/* Criando uma Aplicação de Endereços */
+python manage.py startapp enderecos
+
+/* Criando uma Migração */
+python manage.py makemigrations
+
+/* Migrando para o Banco de Dados */
+python manage.py migrate
+
+/* Criando Migração */
+python manage.py makemigrations
+
+'''
