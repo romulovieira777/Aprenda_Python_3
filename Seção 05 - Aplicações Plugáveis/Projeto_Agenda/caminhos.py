5a7ca7545961c164f35d95a96d36e061cc3b30cd
romulovieira777 <romulo.vieira777@gmail.com>
2021-03-29 09:12:07 -0300

    Aplicações Plugáveis

---
+++ Seção 05 - Aplicações Plugáveis/Projeto_Agenda/caminhos.py
@@ -0,0 +1,20 @@
+"""
+/* Criando um Projeto */
+django-admin startproject agenda .
+
+/* Criando um Banco de Dados */
+python manage.py migrate
+
+/* Criando Superusuario */
+python manage.py createsuperuser
+
+/* Levantando Aplicação */
+python manage.py runserver
+
+/* Criando uma Migração */
+python manage.py makemigrations
+
+/* Fazendo a Migração */
+python manage.py migrate
+
+"""
---
+++ Seção 05 - Aplicações Plugáveis/Projeto_Agenda/pessoas/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from pessoas.models import Pessoa, Telefone
+
+admin.site.register(Pessoa)
+admin.site.register(Telefone)
